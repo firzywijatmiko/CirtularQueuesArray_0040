6b392d3550b8fc9cf85ae2fe7ecf752998bb89ef
firzy <firzy.wijatmiko.ft23@mail.umy.ac.id.com>
2024-05-30 18:57:58 +0700

    cout << "menu" << endl; cout << "1. implement insert operation" << endl; cout << "2. implement insert operation" << endl; cout << "3. display values" << endl; cout << "4. exit" << endl; cout << "enter your choice (1-4): ";

--- CirtularQueuesArray_0040/CirtularQueuesArray_0040.cpp
+++ CirtularQueuesArray_0040/CirtularQueuesArray_0040.cpp
@@ -56,54 +56,81 @@ public:
 				FRONT = FRONT + 1;
 		}
 	}
-
-	void dsplay() {
+	void display() {
 		int FRONT_position = FRONT;
 		int REAR_position = REAR;
 
 		//cek apakah antrian kosong 
 		if (FRONT == -1) {
-			cout << "Quesu is empty\n";
+			cout << "queue is empty\n";
 			return;
 		}
 
-		cout << "\nlEmen   the queue...\n";
-		// jika front _ position <==rear_positin, interasi dari front himgga rear
+		cout << "\nelements in the queue are...\n";
+
+		//jika FRONT_position <= REAR_position, iterasi dari FRONT hingga REAR
 		if (FRONT_position <= REAR_position) {
-			while (FRONT_position <= REAR_postion) {
-				cout << queue_array[FRONT_position] << "   ";
+			while (FRONT_position <= REAR_position) {
+				cout << queue_array[FRONT_position] << " ";
 				FRONT_position++;
 			}
 			cout << endl;
 		}
 		else {
-			// Jika FRONT_position <= REAR_postion, iterasi dari FRONT hingga REAR
+			// jika FRONT_position > REAR_position, iterasi dari FRONT hingga akhir array
 			while (FRONT_position <= max - 1) {
-				cout << queue_array[FRONT_position] << "   ";
-				FRONT_position++;
-			}
-
-			FRONT_position = 0;
-
-			// Iterasi dari awal array hingga REAR
-			while (FRONT_position <= REAR_postion) {
-				cout << queue_array[FRONT_position] << "   ";
+				cout << queue_array[FRONT_position] << " ";
 				FRONT_position++;
 			}
 			cout << endl;
 		}
-	};
+	}
+};
 
-		int main(){
-				Quesues q;
-				char ch;
+int main() {
+	queue q;
+	char ch;
 
-				while (true) {
-					try {
-						cout << "menu" << endl;
-						cout << "1. implement insert opration" << endl;
-						cout << "2.implement insert opration" << endl;
-						cout << "3. dispaly values" << endl;
-						cout << "4.  exit" << endl;
-						cout << "5, enter your choise (1-4):" << endl;
+	while (true) {
+		try {
+			cout << "menu" << endl;
+			cout << "1. implement insert operation" << endl;
+			cout << "2. implement insert operation" << endl;
+			cout << "3. display values" << endl;
+			cout << "4. exit" << endl;
+			cout << "enter your choice (1-4): ";
+			cin >> ch;
+			cout << endl;
+			switch (ch) {
+			case '1': {
+
+				q.insert();
+				break;
+
+			}
+			case '2': {
+				q.remove();
+				break;
+			}
+			case '3': {
+				q.display();
+				break;
+			}
+			case '4': {
+				return 0;
+			}
+			case '5': {
+				cout << "invalid option!!" << endl;
+				break;
+			}
+			}
+		}
+		catch (exception& e) {
+			cout << "chechk for the values entered." << endl;
+		}
+	}
+
+	return 0;
+};
+
 							
