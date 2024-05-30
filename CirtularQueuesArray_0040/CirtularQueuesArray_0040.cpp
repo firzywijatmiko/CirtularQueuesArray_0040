74a6b49fd55d789c1de36031465d6adfe3cb91e8
firzy <firzy.wijatmiko.ft23@mail.umy.ac.id.com>
2024-05-30 19:02:57 +0700

    default: {     cout << "Invalid option!!" << endl;     break; }

--- CirtularQueuesArray_0040/CirtularQueuesArray_0040.cpp
+++ CirtularQueuesArray_0040/CirtularQueuesArray_0040.cpp
@@ -38,7 +38,7 @@ public:
 	};
 	void remove() {
 		// cek apakah antrian kosong
-		if (FRONT == -1){
+		if (FRONT == -1) {
 
 		}
 		cout << "nThe element deleted from the Queue is; " << queue_array[FRONT] << "\n";
@@ -55,82 +55,86 @@ public:
 			else
 				FRONT = FRONT + 1;
 		}
-	}
+	};
 	void display() {
 		int FRONT_position = FRONT;
-		int REAR_position = REAR;
+		int REAR_postion = REAR;
 
-		//cek apakah antrian kosong 
+		// Cek apakah antrian kosong
 		if (FRONT == -1) {
-			cout << "queue is empty\n";
+			cout << "Queue is empty\n";
 			return;
 		}
 
-		cout << "\nelements in the queue are...\n";
+		cout << "\nElements in the queue are...\n";
 
-		//jika FRONT_position <= REAR_position, iterasi dari FRONT hingga REAR
-		if (FRONT_position <= REAR_position) {
-			while (FRONT_position <= REAR_position) {
-				cout << queue_array[FRONT_position] << " ";
+		// Jika FRONT_postion > REAR_position, iterasi dari FRONT hingga akhir array
+		if (FRONT_position <= REAR_postion) {
+			while (FRONT_position <= REAR_postion) {
+				cout << queue_array[FRONT_position] << "   ";
 				FRONT_position++;
 			}
 			cout << endl;
 		}
 		else {
-			// jika FRONT_position > REAR_position, iterasi dari FRONT hingga akhir array
+			// Jika FRONT_position <= REAR_postion, iterasi dari FRONT hingga REAR
 			while (FRONT_position <= max - 1) {
-				cout << queue_array[FRONT_position] << " ";
+				cout << queue_array[FRONT_position] << "   ";
+				FRONT_position++;
+			}
+
+			FRONT_position = 0;
+
+			// Iterasi dari awal array hingga REAR
+			while (FRONT_position <= REAR_postion) {
+				cout << queue_array[FRONT_position] << "   ";
 				FRONT_position++;
 			}
 			cout << endl;
 		}
-	}
-};
+	};
+	int main() {
+		Queues q;
+		char ch;
 
-int main() {
-	queue q;
-	char ch;
+		while (true) {
+			try {
+				cout << "Menu" << endl;
+				cout << "1. Implement insert operation" << endl;
+				cout << "2. Implement Delete Operation" << endl;
+				cout << "3. Display values " << endl;
+				cout << "4. Exit " << endl;
+				cout << "Enter your choice (1-4): ";
+				cin >> ch;
+				cout << endl;
 
-	while (true) {
-		try {
-			cout << "menu" << endl;
-			cout << "1. implement insert operation" << endl;
-			cout << "2. implement insert operation" << endl;
-			cout << "3. display values" << endl;
-			cout << "4. exit" << endl;
-			cout << "enter your choice (1-4): ";
-			cin >> ch;
-			cout << endl;
-			switch (ch) {
-			case '1': {
+				switch (ch) {
+				case '1': {
 
-				q.insert();
-				break;
-
+					q.insert();
+					break;
+				}
+				case '2': {
+					q.remove();
+					break;
+				}
+				case '3': {
+					q.display();
+					break;
+				}
+				case '4': {
+					return 0;
+				}
+				default: {
+					cout << "Invalid option!!" << endl;
+					break;
+				}
+				}
 			}
-			case '2': {
-				q.remove();
-				break;
-			}
-			case '3': { 
-				q.display();
-				break;
-			}
-			case '4': {
-				return 0;
-			}
-			case '5': {
-				cout << "invalid option!!" << endl;
-				break;
-			}
+			catch (exception& e) {
+				cout << "Check for the values entered." << endl;
 			}
 		}
-		catch (exception& e) {
-			cout << "chechk for the values entered." << endl;
-		}
-	}
 
-	return 0;
-};
-
-							
+		return 0;
+	};
